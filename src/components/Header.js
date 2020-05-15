cd15e53e060544bcc88dc3a1d0bf30f5d529141b
Daniel Trujillo <danielfetrujillo@gmail.com>
2020-05-15 12:05:47 -0500

    fixed scrollspy

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,10 @@
 import React from 'react'
 
-import logo from '../assets/images/logo.svg';
 import logo2 from '../assets/images/logo2.svg';
-import logo3 from '../assets/images/logo3.svg';
 
 const Header = (props) => (
     <header id="header" className="alt">
-        <span className="logo"><img src={logo3} style={{width: 85}}alt="" /></span>
+        <span className="logo"><img src={logo2} style={{width: 85}}alt="" /></span>
         <h1>Daniel Felipe Trujillo</h1>
         <p>Mechanical Engineer |  Web Developer</p>
     </header>
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,7 +4,7 @@ import Scroll from './Scroll'
 
 const Nav = (props) => (
     <nav id="nav" className={props.sticky ? 'alt' : ''}>
-        <Scrollspy items={ ['intro', 'first', 'second', 'cta'] } currentClassName="is-active" offset={-300}>
+        <Scrollspy items={ ['intro', 'first', 'second', 'third', 'fourth'] } currentClassName="is-active" offset={-300}>
             <li>
                 <Scroll type="id" element="intro">
                     <a href="#">About me</a>
